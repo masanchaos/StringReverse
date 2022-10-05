91be2a7034430f30f7f8964a998de87ae049bc09
masanchaos <jeffma0831@gmail.com>
2022-10-05 20:47:45 +0800

    first commit test

---
+++ src/main/java/TestReverse.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class TestReverse {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("请输入你想反转的字符串:");
+//        String str = input.next();
+//        String strReverse = stringReverse(str);
+//        System.out.println("反转后的字符串=" + strReverse);
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入你想反转的字符串:");
+        String str = input.next();
+        String s = stringReverseByNum(str, 3, 6);
+        System.out.println("部分反转后的字符串=" + s);
+    }
+
+    //全反转111
+    public static String stringReverse(String str){
+        StringBuffer stringBuffer = new StringBuffer(str);
+        StringBuffer strReverse = stringBuffer.reverse();
+        String strReverseString = strReverse.toString();
+        return strReverseString;
+    }
+
+    //部分反转
+    public static String stringReverseByNum(String str , int start , int end){
+        if(str.toCharArray().length < end){
+            return "字符串长度不够";
+        }
+        StringBuilder fullStr = new StringBuilder(str);
+        String middle = new StringBuilder(str.substring(start, end)).reverse().toString();
+        return fullStr.replace(start, end, middle).toString();
+
+
+    }
+
+
+
+}
